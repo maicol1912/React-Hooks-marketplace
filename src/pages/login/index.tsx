e7c77fadd09a37f1bd6d9239085cb81aae4c08b8
maicol1912 <maicolarcila34@gmail.com>
2023-04-21 15:03:59 -0500

    se configura las validaciones con formil y yup

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -11,90 +11,93 @@ import {
 import {} from '@mui/material';
 import { UseNotification } from '../../context/notification.context';
 import { LoginValidate } from '../../utils/validateForm';
+import { useFormik } from 'formik';
 
 type loginType = {
-  username:string;
-  password:string;
+  username: string;
+  password: string;
 };
+
 export const LoginPage: React.FC<{}> = () => {
-  //TODO: usamos el metodo getError de useNotification
-  const { getError, getSuccess } = UseNotification();
-  
-  //definimos un login data que es el estado anterior y el setLoginData que es el nuevo estado
-  const [loginData, setLoginData] = React.useState<loginType>({
-    username: '',
-    password: '',
+  const { getSuccess } = UseNotification();
+  //TODO: creamos una constante de formik, usamos el useFormik y le mandamos el tipo de fields que este debe tener
+  const formik = useFormik<loginType>({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    //TODO: le enviamos el tipo validador de YUP / definido en carpeta utils
+    validationSchema: LoginValidate,
+    onSubmit: (values) => {
+      //TODO: al enviar y pasar todas las validaciones hara el getSuccess
+      getSuccess(JSON.stringify(values));
+    },
   });
 
-  //hacemos que cada que el textField cambie entonces el estado se actualize de lo que habia a lo que hay ahora
-  const dataLogin = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLoginData({ ...loginData, [e.target.name]: e.target.value });
-  };
-
-  //cuando se envia el formulario obtenemos el loginData que es el formulario ya actualizado
-  const handleSumbit = (e: React.FormEvent<HTMLInputElement>) => {
-    //PreventDefault evita que la pagina se refesque y se pierdan los cambios
-    e.preventDefault();
-    LoginValidate.validate(loginData).then(()=>{
-        getSuccess(JSON.stringify(loginData));
-    }).catch((error:any)=>{
-        getError(error.message)
-    })
-    
-  };
-  return (
-    <Container maxWidth="sm">
-      {/*El sx es usado para marcar los estilos de css dentro del codigo*/}
-      <Grid
-        container
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        sx={{ minHeight: '100vh' }}
-      >
-        <Grid item>
-          {/*El paper es un div parecido al color de fondo pero no es el coloro de BG como tal*/}
-          <Paper sx={{ padding: '1.2em', borderRadius: '0.5em' }}>
-            {/*el box permite comportarse de difernete manera de componentes, en este caso se comporta como form*/}
-            <Typography sx={{ mt: 1, mb: 1 }} variant="h4">
+return (
+  <Container maxWidth="sm">
+    {/*El sx es usado para marcar los estilos de css dentro del codigo*/}
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: '100vh' }}
+    >
+      <Grid item>
+        {/*El paper es un div parecido al color de fondo pero no es el coloro de BG como tal*/}
+        <Paper sx={{ padding: '1.2em', borderRadius: '0.5em' }}>
+          {/*el box permite comportarse de difernete manera de componentes, en este caso se comporta como form*/}
+          <Typography sx={{ mt: 1, mb: 1 }} variant="h4">
+            Iniciar Sesion
+          </Typography>
+          {/*manejamos el sumbit por medio del handleSubmit de formik*/}
+          <Box component="form" onSubmit={formik.handleSubmit}>
+            <TextField
+              name="username"
+              margin="normal"
+              type="text"
+              fullWidth
+              label="username"
+              sx={{ mt: 2, mb: 1.5 }}
+              //TODO: le enviamos el valor del field
+              value={formik.values.username}
+              //TODO: le manejamos el change de formik
+              onChange={formik.handleChange}
+              //TODO: manejamos los errores de formik con el nombre del field
+              error={formik.touched.username && Boolean(formik.errors.username)}
+              //TODO: enviamos el helperText y el nombre del field
+              helperText={formik.touched.username && formik.errors.username}
+            />
+            <TextField
+              name="password"
+              margin="normal"
+              type="password"
+              fullWidth
+              label="Password"
+              sx={{ mt: 1.5, mb: 1.5 }}
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              /*si esta tocando el textField con touched vamos a pasar el error*/
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              /*si esta tocando el textField con touched vamos a pasar el error*/
+              helperText={formik.touched.password && formik.errors.password}
+            />
+            <Button
+              fullWidth
+              type="submit"
+              variant="contained"
+              sx={{ mt: 1.5, mb: 2 }}
+            >
               Iniciar Sesion
-            </Typography>
-            <Box component="form" onSubmit={handleSumbit}>
-              <TextField
-                name="username"
-                margin="normal"
-                type="text"
-                fullWidth
-                label="Email"
-                sx={{ mt: 2, mb: 1.5 }}
-                required
-                onChange={dataLogin}
-              />
-              <TextField
-                name="password"
-                margin="normal"
-                type="password"
-                fullWidth
-                label="Password"
-                sx={{ mt: 1.5, mb: 1.5 }}
-                required
-                onChange={dataLogin}
-              />
-              <Button
-                fullWidth
-                type="submit"
-                variant="contained"
-                sx={{ mt: 1.5, mb: 2 }}
-              >
-                Iniciar Sesion
-              </Button>
-            </Box>
-          </Paper>
-        </Grid>
+            </Button>
+          </Box>
+        </Paper>
       </Grid>
-      <Button fullWidth variant="contained">
-        Estamos en login
-      </Button>
-    </Container>
-  );
+    </Grid>
+    <Button fullWidth variant="contained">
+      Estamos en login
+    </Button>
+  </Container>
+);
 };
--- src/utils/validateForm.ts
+++ src/utils/validateForm.ts
@@ -2,6 +2,6 @@ import * as yup from "yup"
 
 //SE usa para validar los datos de los formuarios
 export const LoginValidate = yup.object().shape({
-    username:yup.string().trim().required("The username is required"),
-    password: yup.string().trim().required("The password is required")
+    username:yup.string().trim().required("The username is required").min(4,"el minimo debe ser 4 caracteres").max(10,"el maximo es de 10 caracteres"),
+    password: yup.string().trim().required("The password is required").min(4, "el minimo debe ser 4 caracteres").max(10, "el maximo es de 10 caracteres"),
 })
